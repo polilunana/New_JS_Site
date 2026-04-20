4ab107d941876e180be5c6e1fef77bb4f7ff5fbd
polilunana <polina.vedulina@gmail.com>
2026-04-20 12:58:02 +0300

    update

--- js/utils.js
+++ js/utils.js
@@ -4,14 +4,14 @@ function createProductCard(p) {
     a.href = "#";
     a.innerHTML = 
         '<div class="product-img">' + p.emoji + "</div>" +
-        '<div class="product-info>' +
+        '<div class="product-info">' +
             '<div class="product-name">' + p.name + "</div>" +
             '<div class="product-category">' + p.category + "</div>" +
             '<div class="product-footer">' +
                 '<div class="product-price">' + p.price + "</div>" +
                 '<button class="btn-add">' + "Кошик" + "</button>" +
-            "</div>"
-        "</div>"
+            "</div>" +
+        "</div>";
 
     return a;
 }
